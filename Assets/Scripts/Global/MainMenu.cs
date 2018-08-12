c1e8e4434567d23c318834d0ba8803e132d78c5e
behistun <jrl15b@acu.edu>
2018-08-12 10:02:47 -0500

    Level 3 Add
    Added all Level 3 Components

--- Assets/Scripts/Global/MainMenu.cs
+++ Assets/Scripts/Global/MainMenu.cs
@@ -4,33 +4,43 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
-{
-    //vars
-    [SerializeField]
-    Text versionText;
-    string gameText;
-    
-
-    //Main Menu scene control system
-
-    void Start()
+namespace Assets.Scripts.VersionEditor {
+    public class MainMenu : MonoBehaviour
     {
-        gameText = Application.version;
-        
-    }
+        //vars
+        [SerializeField]
+        Text versionText;
 
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1); //temp, will eventually load a scene that generates the level map. Currently loads the test level.
-        Debug.Log("Loading temp scene");
-    }
 
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Quit the Game");
+
+
+        //Main Menu scene control system
+
+        void Start()
+        {
+            VersionText();
+
+        }
+
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene(1); //temp, will eventually load a scene that generates the level map. Currently loads the test level.
+            Debug.Log("Loading temp scene");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+            Debug.Log("Quit the Game");
+        }
+
+        void VersionText()
+        {
+            Debug.Log("Version Text Display");
+            versionText.text = VersionInformation.ToString();
+        }
+
     }
-	
 }
---
+++ Assets/Scripts/VersionEditor/VersionInformation.cs
@@ -0,0 +1,17 @@
+/*Auto-Generated File from VersionEditor 2018.08.12 10:00:01 */
+
+namespace Assets.Scripts.VersionEditor {
+	public static class VersionInformation {
+		public new static string ToString() {
+			return string.Format("{0}.{1}.{2}.{3}", Major, Minor, Revision, Build);
+		}
+		public const int Major = 0;
+		public const int Minor = 2;
+		public const int Revision = 4;
+		public const string Build = @"0";
+
+		public const string Title = "2d Platformer";
+		public const string Company = "SpectralFlameGames";
+	}
+}
+
