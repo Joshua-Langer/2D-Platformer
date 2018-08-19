0ca9ae8f090a94ae45ce49e0f220a9937f67b503
behistun <jrl15b@acu.edu>
2018-08-19 16:33:44 -0500

    Initial Commit
    Adding all files and setting up for adding multiplayer

--- Assets/Scripts/VersionEditor/VersionInformation.cs
+++ Assets/Scripts/VersionEditor/VersionInformation.cs
@@ -1,4 +1,4 @@
-/*Auto-Generated File from VersionEditor 2018.08.17 23:09:11 */
+/*Auto-Generated File from VersionEditor 2018.08.18 17:21:04 */
 
 namespace Assets.Scripts.VersionEditor {
 	public static class VersionInformation {
@@ -6,9 +6,9 @@ namespace Assets.Scripts.VersionEditor {
 			return string.Format("{0}.{1}.{2}.{3}", Major, Minor, Revision, Build);
 		}
 		public const int Major = 0;
-		public const int Minor = 3;
-		public const int Revision = 3;
-		public const string Build = @"293015";
+		public const int Minor = 4;
+		public const int Revision = 0;
+		public const string Build = @"8994666";
 
 		public const string Title = "Wizard Saves All";
 		public const string Company = "SpectralFlameGames";
